d3372f50b4bf4acf725e1ce915101561fa9ed41e
Julio Foulquié <jfoulquie@gmail.com>
2015-04-13 00:37:41 +0200

    Make some parts static on the library to allow faster execution.

--- src/j3j5/LastfmApio.php
+++ src/j3j5/LastfmApio.php
@@ -21,20 +21,21 @@ class LastfmApio {
 
 	private static $api_url = 'http://ws.audioscrobbler.com/2.0/';
 	private static $auth_url = 'http://www.last.fm/api/auth/';
+	private static $responses = array();
+	private static $log;
 
-	private $api_settings = array();
+	private static $api_settings = array();
 	private $curl_settings = array();
 
 	private $curl;
 	private $rolling_curl;
-	private $max_concurrent_requests = 20;
+	private $max_concurrent_requests = 50;
 
-	private $responses = array();
 
 	public function __construct($api_settings = array()) {
 		include __DIR__ . '/config.php';
-		$this->api_settings = array_merge($this->api_settings, $last_fm_api_settings);
-		$this->api_settings = array_merge($this->api_settings, $api_settings);
+		self::$api_settings = array_merge(self::$api_settings, $last_fm_api_settings);
+		self::$api_settings = array_merge(self::$api_settings, $api_settings);
 
 		$this->curl_settings = array(
 			CURLOPT_SSL_VERIFYPEER	=> FALSE,
@@ -42,39 +43,22 @@ class LastfmApio {
 			CURLOPT_USERAGENT		=> 'RollingCurlX test script',
 		);
 
-		$this->log = new Logger('lastfm-api');
-		if(PHP_SAPI == 'cli') {
-			$this->log->pushHandler(new StreamHandler("php://stdout", Logger::WARNING));
-		} else {
-// 			$this->log->pushHandler(new StreamHandler(dirname(__DIR__) . '/data/logs/last-stream.log', Logger::WARNING));
-		}
+		self::create_log_instance();
 	}
 
 	public function set_api_settings($config) {
 		// The consumer key and secret must always be included when reconfiguring
-		$this->api_settings = array_merge($this->api_settings, $config);
+		self::$api_settings = array_merge(self::$api_settings, $config);
 	}
 
 	public function get_api_settings() {
-		return $this->api_settings;
+		return self::$api_settings;
 	}
 
-	private function _get_curl() {
-		if(empty($this->curl)) {
-			$this->curl = new Curl();
-			foreach($this->curl_settings AS $setting => $value) {
-				$this->curl->setopt($setting, $value);
-			}
-		}
-		return $this->curl;
-	}
-
-	private function _get_rolling_curl() {
-		if(empty($this->rolling_curl)) {
-			$this->rolling_curl = new RollingCurlX($this->max_concurrent_requests);
-			$this->rolling_curl->setOptions($this->curl_settings);
-		}
-		return $this->rolling_curl;
+	public function set_max_concurrent_reqs($max_concurrent_requests) {
+		$this->max_concurrent_requests = intval($max_concurrent_requests);
+		$this->rolling_curl = NULL;
+		$this->_get_rolling_curl();
 	}
 
 	/**
@@ -118,7 +102,7 @@ class LastfmApio {
 				array(
 					'method' => $method,
 					'format' => 'json',
-					'api_key' => $this->api_settings['api_key']
+					'api_key' => self::$api_settings['api_key']
 				),
 				$parameters
 		);
@@ -126,8 +110,8 @@ class LastfmApio {
 		// Do we need to authenticate the request ?
 		if( $do_request_auth ) {
 			// We add the session key if it's been given
-			if( !empty($this->api_settings['session_key']) )
-				$parameters['sk'] = $this->api_settings['session_key'];
+			if( !empty(self::$api_settings['session_key']) )
+				$parameters['sk'] = self::$api_settings['session_key'];
 
 			// Known bug : you have to get rid of format parameter to compute the api_sig parameter.
 			// http://www.lastfm.fr/group/Last.fm+Web+Services/forum/21604/_/428269/1#f18907544
@@ -140,22 +124,50 @@ class LastfmApio {
 			foreach( $parameters_without_format as $k => $v ) {
 				$signature .= "$k$v";
 			}
-			$parameters['api_sig'] = md5($signature . $this->api_settings['api_secret']);
+			$parameters['api_sig'] = md5($signature . self::$api_settings['api_secret']);
 		}
 
 		// We have everything we need, let's query the API
 		$rolling_curl = $this->_get_rolling_curl();
 		$rolling_curl->setHeaders(array('Content-type: application/x-www-form-urlencoded'));
-		$rolling_curl->addRequest(self::$api_url, $parameters, array($this, 'process_response'), $user_parameters);
+		$rolling_curl->addRequest(self::$api_url, $parameters, __NAMESPACE__ .'\LastfmApio::process_response', $user_parameters);
+		self::$log->addDebug("Request added: " . self::$api_url . '?' . http_build_query($parameters));
 		if(!$do_multi_request) {
+			self::$log->addDebug("Single request, running...");
 			$responses = $this->run_multi_requests();
 			return $responses[implode('.', $user_parameters)];
 		}
 	}
 
 	public function run_multi_requests() {
+		self::$log->addDebug("Executing parallel calls");
 		$this->_get_rolling_curl()->execute();
 		return $this->get_responses();
+	}
+
+	private function get_responses() {
+		return self::$responses;
+	}
+
+	private function _get_rolling_curl() {
+		if(empty($this->rolling_curl)) {
+			$this->rolling_curl = new RollingCurlX($this->max_concurrent_requests);
+			self::$log->addDebug("New RollingCurlX created with {$this->max_concurrent_requests} max concurrent reqs.");
+			$this->rolling_curl->setOptions($this->curl_settings);
+		}
+		return $this->rolling_curl;
+	}
+
+	public static function create_log_instance() {
+		if(empty(self::$log)) {
+			///WARNING: Setting this to a level lower level than warning can slow things down.
+			self::$log = new Logger('lastfm-apio');
+			if(PHP_SAPI == 'cli') {
+				self::$log->pushHandler(new StreamHandler("php://stdout", Logger::WARNING));
+			} else {
+				// 			self::$log->pushHandler(new StreamHandler(dirname(__DIR__) . '/data/logs/last-stream.log', Logger::WARNING));
+			}
+		}
 	}
 
 	/**
@@ -164,26 +176,93 @@ class LastfmApio {
 	 * @throws Exception if something goes wrong.
 	 *
 	 */
-	public function process_response($response, $url, $request_info, $parameters, $time) {
+	public static function process_response($response, $url, $request_info, $parameters, $time) {
+		self::create_log_instance();
+		self::$log->addDebug("Processing response for: " . $url . " with parameters " . print_r($parameters, TRUE) );
+
 		if ($request_info['http_code'] !== 200) {
-			throw new \Exception("Connection error " . $request_info['http_code']);
+			self::$log->addError("Connection error " . $request_info['http_code']);
+			self::$responses[implode('.', $parameters)] = FALSE;
+			return;
 		}
 
 		$json = json_decode( $response );
 
 		// The JSON couldn't be decoded …
-		if( $json === false )
-			throw new \Exception("JSON response seems incorrect.");
+		if( $json === false ) {
+			self::$log->addError("JSON response seems incorrect:");
+			self::$log->addError($response);
+			self::$responses[implode('.', $parameters)] = FALSE;
+			return;
+		}
 
 		// An error has occurred …
-		if( !empty($json->error) )
-			throw new \Exception("[{$json->error}|{$json->message}] "
-			.implode(', ', $json->links)."\n".http_build_query( $parameters ));
-		$this->responses[implode('.', $parameters)] = $json;
+		if( !empty($json->error) ) {
+			self::handle_api_errors($json, $parameters);
+			self::$responses[implode('.', $parameters)] = FALSE;
+			return;
+		}
+		self::$responses[implode('.', $parameters)] = $json;
 	}
 
-	private function get_responses() {
-		return $this->responses;
+	private static function handle_api_errors($json, $parameters) {
+		self::create_log_instance();
+		$parameters = array_merge(
+			array(
+				'format' => 'json',
+				'api_key' => self::$api_settings['api_key']
+			),
+			$parameters
+		);
+		switch($json->error) {
+		// 8 : Operation failed - Something else went wrong
+			case 8:
+			// 16 : There was a temporary error processing your request. Please try again
+			case 16:
+				// Esto peeeetaaaa (error that the API throws on some legit calls, just ignore it)
+				self::$log->addError("API call failed with error({$json->error}): http://ws.audioscrobbler.com/2.0/?" . http_build_query( $parameters ));
+				self::$log->addError($json->message);
+				return;
+			// 29 : Rate limit exceeded - Your IP has made too many requests in a short period
+			case 29:
+				self::$log->addError("Rate limit hit on: http://ws.audioscrobbler.com/2.0/?" . http_build_query( $parameters ));
+				self::$log->addError($json->message);
+				self::$log->addError("Exiting.");
+				exit;
+			// 10 : Invalid API key - You must be granted a valid key by last.fm
+			case 10:
+				self::$log->addError("Invalid API key, go to http://www.last.fm/api/accounts and get a valid one.");
+				exit;
+			// 2 : Invalid service - This service does not exist
+			case 2:
+			// 3 : Invalid Method - No method with that name in this package
+			case 3:
+			// 4 : Authentication Failed - You do not have permissions to access the service
+			case 4:
+			// 5 : Invalid format - This service doesn't exist in that format
+			case 5:
+			// 6 : Invalid parameters - Your request is missing a required parameter
+			case 6:
+			// 7 : Invalid resource specified
+			case 7:
+			// 9 : Invalid session key - Please re-authenticate
+			case 9:
+			// 11 : Service Offline - This service is temporarily offline. Try again later.
+			case 11:
+			// 13 : Invalid method signature supplied
+			case 13:
+			// 26 : Suspended API key - Access for your account has been suspended, please contact Last.fm
+			case 26:
+			default:
+				self::$log->addError('UNHANDLED ERROR: ' );
+				self::$log->addError("API call failed: http://ws.audioscrobbler.com/2.0/?" . http_build_query( $parameters ));
+				self::$log->addError(print_r($json, TRUE));
+				exit;
+				break;
+		}
+		self::$log->addError('UNKNOWN ERROR: ' . $json->message );
+		self::$log->addError("API call failed: http://ws.audioscrobbler.com/2.0/?" . http_build_query( $parameters ));
+		exit;
 	}
 
 }
