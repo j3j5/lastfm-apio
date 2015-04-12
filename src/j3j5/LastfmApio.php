b5f9672955c7ff6274ade9546f5104f2e6129377
Julio Foulquié <jfoulquie@gmail.com>
2015-04-12 19:02:02 +0200

    Add LastfmApio class.

---
+++ src/j3j5/LastfmApio.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * LastfmApio
+ *
+ * A wrapper to make easier to use Twitter's API with tmhOAuth library.
+ *
+ * @author Julio Foulquié
+ * @version 0.1.0
+ *
+ * 06 Mar 2015
+ */
+
+namespace j3j5;
+
+class LastfmApio {
+
+	private static $debug = FALSE;
+
+	protected $api_settings;
+
+	public function _construct() {
+
+	}
+
+}
