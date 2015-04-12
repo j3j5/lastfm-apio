37de7055b37968977a4b972e59a04460bfa9e09f
Julio Foulquié <jfoulquie@gmail.com>
2015-04-13 00:38:06 +0200

    Improve the current test.

--- tests/get_weekly_artist_chart_multi_request.php
+++ tests/get_weekly_artist_chart_multi_request.php
@@ -11,6 +11,9 @@ $api->user_getweeklyartistchart(array('user' => $username, 'from' => 1210507200,
 $api->user_getweeklyartistchart(array('user' => $username, 'from' => 1217764800, 'to' => 1218369600), FALSE, TRUE);
 $api->user_getweeklyartistchart(array('user' => $username, 'from' => 1232280000, 'to' => 1232884800), FALSE, TRUE);
 
-$response = $api->run_multi_requests();
-
-var_dump($response);
+// The response to all concurrent requests is return as an array using as key a string of the parameters joined by '.'
+$responses = $api->run_multi_requests();
+foreach($responses AS $response) {
+	$allresponseresults = isset($response->weeklyartistchart->artist) ? $response->weeklyartistchart->artist : array();
+	print count($allresponseresults) . " artists found." . PHP_EOL;
+}
